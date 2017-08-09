b3865635f24140823bdc52e8b547081438ad9712
pradeep <pradeep.klumech@gmail.com>
2017-08-09 07:10:10 +0530

    commit

---
+++ Top10Songs_Prediction.R
@@ -0,0 +1,25 @@
+Songs = read.csv(choose.files())
+table(Songs$year == 2010)
+table(Songs$artistname == "Michael Jackson")
+MichaelJackson = subset(Songs, Songs$artistname == "Michael Jackson")
+table(MichaelJackson$Top10 == 1)
+MichaelJackson[c("songtitle", "Top10")]
+table(Songs$timesignature)
+which.max(Songs$tempo)
+Songs$songtitle[6206]
+SongsTrain = subset(Songs, Songs$year <= 2009)
+SongsTest = subset(Songs, Songs$year > 2009)
+nonvars = c("year", "songtitle", "artistname", "songID", "artistID")
+SongsTrain = SongsTrain[ , !(names(SongsTrain) %in% nonvars) ]
+SongsTest = SongsTest[ , !(names(SongsTest) %in% nonvars) ]
+SongsLog1 = glm(Top10 ~ ., data=SongsTrain, family=binomial)
+summary(SongsLog1)
+cor(SongsTrain$loudness, SongsTrain$energy)
+SongsLog2 = glm(Top10 ~ . - loudness, data=SongsTrain, family=binomial)
+summary(SongsLog2)
+SongsLog3 = glm(Top10 ~ . - energy, data=SongsTrain, family=binomial)
+summary(SongsLog3)
+TestPrediction = predict(SongsLog3, newdata = SongsTest, type = "response")
+table(SongsTest$Top10, TestPrediction >= 0.45)
+table(SongsTest$Top10)
+table(SongsTest$Top10, TestPrediction >= 0.45)
